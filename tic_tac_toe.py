55a1437fa4b7d59eb50f0d2e11deef9241e21f77
jbian92 <jujuzb88@gmail.com>
2020-07-09 14:20:33 -0400

    edit

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -16,16 +16,32 @@ def place_piece(piece, row, col):
 def has_won_vertical():
     if board[0][0] == "X" and board[1][0] == "X" and board[2][0] == "X":
         print("Player1 won")
+        return True
     elif board[0][1] == "X" and board[1][1] == "X" and board[2][1] == "X":
         print("Player1 won")
+        return True
     elif board[0][2] == "X" and board[1][2] == "X" and board[2][2] == "X":
         print("Player1 won")
+        return True
     elif board[0][0] == "O" and board[1][0] == "O" and board[2][0] == "O":
         print("Player2 won")
+        return True
     elif board[0][1] == "O" and board[1][1] == "O" and board[2][1] == "O":
         print("Player2 won")
+        return True
     elif board[0][2] == "O" and board[1][2] == "O" and board[2][2] == "O":
         print("Player2 won")
+        return True
+    else:
+        return False
+
+def has_won_horizontal():
+    for i in range(len(board)):
+        if board[i].count("X") == 3:               
+            return True
+        elif board[i].count("O") == 3:
+            return True
+    return False
 
 def tic_tac_toe():
     turn = random.random()
@@ -35,8 +51,7 @@ def tic_tac_toe():
     elif turn < 0.5:
         print("It's Player 1's turn.")
         turn = 1
-##    while (!has_won_vertical() or !has_won_diagonal() or !has_won_horizontal()):
-while True:
+while (!has_won_vertical() or !has_won_diagonal() or !has_won_horizontal()):
     if count % 2 == 1:
         choice = input("Where do you want to put down X? Format: X, row, col")
         placements = choice.split(",")
@@ -45,14 +60,9 @@ while True:
         choice = input("Where do you want to put down O? Format: O, row, col")
         placements = choice.split(",")
         place_piece(placements[0], int(placements[1]), int(placements[2]))
-    #count+=1
+    count+=1
 
     has_won_vertical()
+    has_won_horizontal()
 
 tic_tac_toe()
-
-#def has_won_horizontal():
-#def has_won_diagonal():
-
-
-
