917d661e0485abc398df9a2ebc0dca61b261ee86
jbian92 <jujuzb88@gmail.com>
2020-07-09 14:37:37 -0400

    edit

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -4,15 +4,9 @@ board = [
     ["", "", ""], 
     ["", "", ""]
     ]
-#player1 = input("What is Player 1's Name? ")
-#player2 = input("What is Player 2's Name? ")
-count = 1
-#winner = 0
-
 def place_piece(piece, row, col):
     board[row-1][col-1] = piece
     print(board)
-
 def has_won_vertical():
     if board[0][0] == "X" and board[1][0] == "X" and board[2][0] == "X":
         print("Player1 won")
@@ -34,35 +28,25 @@ def has_won_vertical():
         return True
     else:
         return False
-
 def has_won_horizontal():
     for i in range(len(board)):
         if board[i].count("X") == 3:               
+            print("Player1 won")
             return True
         elif board[i].count("O") == 3:
+            print("Player2 won")
             return True
     return False
-
 def tic_tac_toe():
-    turn = random.random()
-    if turn > 0.5:
-        print("It's Player 2's turn.")
-        turn = 2 
-    elif turn < 0.5:
-        print("It's Player 1's turn.")
-        turn = 1
-while (!has_won_vertical() or !has_won_diagonal() or !has_won_horizontal()):
-    if count % 2 == 1:
-        choice = input("Where do you want to put down X? Format: X, row, col")
-        placements = choice.split(",")
-        place_piece(placements[0], int(placements[1]), int(placements[2]))
-    else:
-        choice = input("Where do you want to put down O? Format: O, row, col")
-        placements = choice.split(",")
-        place_piece(placements[0], int(placements[1]), int(placements[2]))
-    count+=1
-
-    has_won_vertical()
-    has_won_horizontal()
-
+    count = 1
+    while (has_won_vertical()==False or has_won_horizontal()==False):
+        if count % 2 == 1:
+            choice = input("Where do you want to put down X? Format: X, row, col")
+            placements = choice.split(",")
+            place_piece(placements[0], int(placements[1]), int(placements[2]))
+        else:
+            choice = input("Where do you want to put down O? Format: O, row, col")
+            placements = choice.split(",")
+            place_piece(placements[0], int(placements[1]), int(placements[2]))
+        count+=1
 tic_tac_toe()
